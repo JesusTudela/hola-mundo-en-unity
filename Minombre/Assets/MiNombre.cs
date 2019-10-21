2cecdbc3ef6a6bdc3fa1be84fe2c8558acea4c84
JesusTudela <jesustudela@ceitartessos.com>
2019-10-21 19:54:24 +0200

    Segundo trabajo en unity

---
+++ Minombre/Assets/MiNombre.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiNombre : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Start () {
+		Debug.Log ("Jesus Tudela Navarro");
+	}
+	// Update is called once per frame
+
+	void Update () {
+		Debug.Log ("19");
+	}
+
+}
